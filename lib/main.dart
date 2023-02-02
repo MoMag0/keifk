9a8de0b6d15b1138fe6dc2fc0adbe77921d5ffd6
kp <sii2moh@gmail.com>
2023-02-02 21:33:59 +0300

    'untest'

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Keifk-enta',
+      title: 'Keifk',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
