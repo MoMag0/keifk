876a8afed66f196b3f6670082cb1adc7b7a3a7c5
kp <sii2moh@gmail.com>
2023-02-11 00:49:06 +0300

    redesign

--- lib/widgets/messages_screen_components/his_message_card.dart
+++ lib/widgets/messages_screen_components/his_message_card.dart
@@ -13,41 +13,60 @@ class HisMessageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: ConstrainedBox(
-        constraints:
-            BoxConstraints(maxWidth: MediaQuery.of(context).size.width - 150),
-        child: Card(
-          elevation: 1,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          color: senderMessageColor,
-          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 10,
-              right: 30,
-              top: 5,
-              bottom: 20,
-            ),
-            child: Column(
-              children: [
-                Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(hisText, style: const TextStyle(fontSize: 16))),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Text(hisMail,
-                      style:
-                          const TextStyle(fontSize: 13, color: Colors.white60)),
+    // we use this column to use crossAxixAlignment attribute
+    return Column(
+      // this attribute to make sender widget most left
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // to make the widget have maximum width with respect to screen size
+        ConstrainedBox(
+          constraints:
+              BoxConstraints(maxWidth: MediaQuery.of(context).size.width - 200),
+          child: Column(
+            // a column contains two widgets ' sender email & its message '
+            crossAxisAlignment: CrossAxisAlignment
+                .start, // we make it most left as well <- why first alignment didnt work?
+            children: [
+              // ## sender email
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 10),
+                child: Text(
+                  hisMail,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
                 ),
-              ],
-            ),
+              ),
+              // ## Sender message
+              Card(
+                elevation: 4,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(0)),
+                ),
+                color: senderMessageColor,
+                margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 10,
+                    right: 15,
+                    top: 10,
+                    bottom: 15,
+                  ),
+                  child: Text(
+                    hisText,
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ),
+      ],
     );
   }
 }
--- lib/widgets/messages_screen_components/my_message_card.dart
+++ lib/widgets/messages_screen_components/my_message_card.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../database/colors.dart';
-
 class MyMessageCard extends StatelessWidget {
   final String myText;
   final String myMail;
@@ -13,41 +11,60 @@ class MyMessageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.centerRight,
-      child: ConstrainedBox(
-        constraints:
-            BoxConstraints(maxWidth: MediaQuery.of(context).size.width - 150),
-        child: Card(
-          elevation: 1,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          color: messageColor,
-          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 10,
-              right: 30,
-              top: 5,
-              bottom: 20,
-            ),
-            child: Column(
-              children: [
-                Align(
-                    alignment: Alignment.topRight,
-                    child: Text(myText, style: const TextStyle(fontSize: 16))),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Text(myMail,
-                      style:
-                          const TextStyle(fontSize: 13, color: Colors.white60)),
+    // we use this column to use crossAxixAlignment attribute
+    return Column(
+      // this attribute to make sender widget most left
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        // to make the widget have maximum width with respect to screen size
+        ConstrainedBox(
+          constraints:
+              BoxConstraints(maxWidth: MediaQuery.of(context).size.width - 200),
+          child: Column(
+            // a column contains two widgets ' my email & my message '
+            crossAxisAlignment: CrossAxisAlignment
+                .end, // we make it most right as well <- why first alignment didnt work?
+            children: [
+              // ## my email
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0, top: 10),
+                child: Text(
+                  myMail,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
                 ),
-              ],
-            ),
+              ),
+              // ## My message
+              Card(
+                elevation: 4,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                      topRight: Radius.circular(0),
+                      topLeft: Radius.circular(20)),
+                ),
+                color: const Color.fromARGB(255, 67, 33, 21),
+                margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 15,
+                    right: 10,
+                    top: 10,
+                    bottom: 15,
+                  ),
+                  child: Text(
+                    myText,
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ),
+      ],
     );
   }
 }
